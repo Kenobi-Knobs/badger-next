36e7b88348da1e2abe2309e1302f0d52b5ecfb00
Konchatniy Vitaliy <konchatniyvitaliy@gmail.com>
2023-01-27 15:15:09 +0200

    add mongodb connection and store users

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 ﻿import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
-import { CheckLoginData } from "../../../lib/telegramVerifyLogin"
+import { CheckLoginData } from "@/lib/telegramVerifyLogin"
+import clientPromise from "@/lib/mongodb";
 
 export const authOptions = {
 	providers: [
@@ -11,6 +12,29 @@ export const authOptions = {
 			async authorize(credentials, req) {
 				const user = CheckLoginData(req.query)
 				if (user.id && user.first_name) {
+					const client = await clientPromise;
+					const db = await client.db();
+					const collection = await db.collection('users');
+					const userExists = await collection.findOne({ id: user.id });
+					if (!userExists) {
+						await collection.insertOne({
+							id: user.id,
+							name: [user.first_name, user.last_name || ''].join(' '),
+							image: user.photo_url,
+							username: user.username,
+							registeredAt: new Date(),
+						});
+					} else {
+						await collection.updateOne(
+							{ id: user.id },
+							{
+								$set: {
+									name: [user.first_name, user.last_name || ''].join(' '),
+									username: user.username,
+									image: user.photo_url,
+								},
+							});
+					}
 					return {
 						id: user.id.toString(),
 						name: [user.first_name, user.last_name || ''].join(' '),
@@ -42,6 +66,9 @@ export const authOptions = {
 		signIn: '/login',
 		signOut: '/login',
 		error: '/login',
+	},
+	jwt: {
+		secret: process.env.JWT_SECRET,
 	},
 	}
 export default NextAuth(authOptions)
